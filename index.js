5018535d87962ee595eaa46ce6c9b604eabe8546
scragonrex <scragondragon@gmail.com>
2023-03-10 15:19:26 +0530

    Update1

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ connectToMongo();
 
 var cors = require('cors')
 const express = require('express')
-const port = process.env.PORT||5000;
+const port = process.env.PORT||3000;
 var app = express()
  
 app.use(cors());
