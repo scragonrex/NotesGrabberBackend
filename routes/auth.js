8b26878d4b4b525ac2114751a54352ddd40203cb
scragonrex <scragondragon@gmail.com>
2023-03-25 09:37:06 +0530

    Added user already exist warning

--- routes/auth.js
+++ routes/auth.js
@@ -16,7 +16,7 @@ router.post('/createuser', [
    body('password', 'Password must be atleast of 5 characters').isLength({ min: 5 }),
    body('email', 'Enter a valid email').isEmail()]
    , async (req, res) => {
-      let success =false;
+      let success ="";
       const errors = validationResult(req);
       if (!errors.isEmpty()) {
          return res.status(400).json({success, errors: errors.array() });
@@ -26,6 +26,7 @@ router.post('/createuser', [
 
          let user = await User.findOne({ email: req.body.email });
          if (user) {
+            success="exist"
             return res.status(400).json({ success, error: "Sorry the user already exist" })
          }
 
@@ -46,7 +47,7 @@ router.post('/createuser', [
             }
          }
          const authToken = jwt.sign(data, JWT_SECRET);
-         success=true;
+         success="ok";
          res.json({ success, authToken });
       } catch (error) {
          console.error(error.message);
